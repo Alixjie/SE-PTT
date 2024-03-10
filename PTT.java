ef612ad9da96ed903adb97f04306addb14901bde
midori-jojo <66398249+midori-jojo@users.noreply.github.com>
2024-03-10 17:50:35 +0000

    Add files via upload

---
+++ PTT.java
@@ -0,0 +1,17 @@
+
+import java.util.*;
+
+
+public class PTT extends staff {
+
+
+    public PTT() {
+    	 this.state = 0; // 将state初始化为0，表示未接受培训
+    }
+
+   
+    public int age;//年龄
+    public String training;//培训细节
+    public int state;//状态
+
+}
